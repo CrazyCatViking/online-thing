4c14a35063b56dff4a5d4259e80f1465b6700369
Kristoffer Solvang-Jansen <krissjansen@gmail.com>
2025-01-04 03:15:05 +0100

    Remove tmp folder

--- client/game/screens/gameBoard.js
+++ client/game/screens/gameBoard.js
@@ -3,10 +3,15 @@ export const createGameBoard = () => {
    * @param {import('../types.d.ts').RenderContext} context 
    */
   const render = (context) => {
-    const { canvas, ctx, gameState } = context;
+    const { canvas, ctx, gameState, input } = context;
 
     if (gameState.state !== 'playing') {
       return;     
+    }
+
+    if (input.keys.has('Escape')) {
+      gameState.state = 'menu';
+      gameState.player = undefined;
     }
 
     ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
