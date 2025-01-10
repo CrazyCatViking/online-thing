1459538e6c4b0600b648628debf029f2c1397cd7
Kristoffer Solvang-Jansen <krissjansen@gmail.com>
2025-01-10 22:55:50 +0100

    Remove text

--- client/game/screens/gameBoard.js
+++ client/game/screens/gameBoard.js
@@ -17,11 +17,6 @@ export const createGameBoard = () => {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = 'white';
-    ctx.font = '48px sans-serif';
-    ctx.textAlign = 'center';
-    ctx.fillText('Naow we pley!', canvas.width / 2, canvas.height / 2);
-
     if (gameState.player !== undefined) {
       gameState.player.render(context);
     }
